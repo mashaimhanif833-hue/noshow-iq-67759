9d439053b112545fc267446730ac1ba15bc941e4
Mashaim hanif <mashaimhanif833@gmail.com>
2026-05-01 17:15:24 -0900

    fix(api): define port variable before app.run

--- noshow_iq/api.py
+++ noshow_iq/api.py
@@ -181,5 +181,6 @@ def stats():
 
 
 if __name__ == '__main__':
-   port = int(os.environ.get('PORT', 7860))
-app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get('PORT', 7860))
+    app.run(host='0.0.0.0', port=port, debug=False)
+    
